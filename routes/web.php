aeaa0366fd13610d54f63854b8e3bdaa45c63b96
premlamsal <premlamsal2@gmail.com>
2019-12-17 21:19:17 +0545

    update

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index');
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
