26e204e9a0a6a697fb752d3416b1a8eea63b7382
premlamsal <premlamsal2@gmail.com>
2019-12-17 21:21:24 +0545

    update the version number

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -43,7 +43,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">{{ config('app.name', 'Laravel') }} <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">{{ config('app.name', 'Laravel') }} <sup>1</sup></div>
       </a>
 
       <!-- Divider -->
